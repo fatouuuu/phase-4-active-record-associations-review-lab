460400e1741efa8372b352cdb86170e6dbff9d91
fatouuuu <fatmaabdullrahim@gmail.com>
2023-03-26 05:54:28 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create some passengers
+passenger1 = Passenger.create(name: "John")
+passenger2 = Passenger.create(name: "Jane")
+passenger3 = Passenger.create(name: "Jack")
+passenger4 = Passenger.create(name: "Jill")
+passenger5 = Passenger.create(name: "Jenny")
+
+# Create some taxis
+taxi1 = Taxi.create(taxi_number: "ABC123")
+taxi2 = Taxi.create(taxi_number: "DEF456")
+taxi3 = Taxi.create(taxi_number: "GHI789")
+taxi4 = Taxi.create(taxi_number: "JKL012")
+taxi5 = Taxi.create(taxi_number: "MNO345")
+
+# Create some rides
+Ride.create(taxi: taxi1, passenger: passenger1)
+Ride.create(taxi: taxi2, passenger: passenger2)
+Ride.create(taxi: taxi3, passenger: passenger3)
+Ride.create(taxi: taxi4, passenger: passenger4)
+Ride.create(taxi: taxi5, passenger: passenger5)
